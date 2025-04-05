19d696509d765694d12543faefdf4cc57b67129b
Danny <svsdinni@gmail.com>
2025-04-05 14:33:28 +0530

    v2

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+	return (
+		<footer className="mt-auto p-4 text-center pt-8">
+			<p>
+				<a
+					href="https://github.com/sahilatahar"
+					target="_blank"
+					rel="noreferrer"
+					className="text-sand-800 hover:underline font-semibold"
+				>
+				</a>{" "}
+			</p>
+		</footer>
+	)
+}
+
+export default Footer
